e3f51f06b30c42b9c19aed0df87c8be940a03bf1
wldgkdl <wldgkdl2@gmail.com>
2021-11-24 15:55:48 -0500

    base html

--- Portfolio.py
+++ Portfolio.py
@@ -43,6 +43,10 @@ app = Flask(__name__)
 app.secret_key = "wjdghks3#"
 
 
+@app.route("/")
+def portfolio():
+    return render_template("index.html")
+
 @app.route("/spec", methods = ["POST", "GET"])
 def spec():
     return render_template("question_form.html")
